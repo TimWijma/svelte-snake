dfc8e37d77873b107b13db7c6d695ac992519f28
Tim Wijma <tim.wijma@gmail.com>
2025-01-13 00:37:29 +0100

    code cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,18 @@
 <script lang="ts">
-    const gridHeight = 10;
-    const gridWidth = 20;
+    const gridHeight = 4;
+    const gridWidth = 4;
 
     let grid = Array(gridHeight * gridWidth).fill(null);
 
-    let snake = [22, 21, 20];
-    let direction = 1;
-
-    const getFood = () => {
-        let newFood = Math.floor(Math.random() * grid.length);
-
-        while (snake.includes(newFood)) {
-            newFood = Math.floor(Math.random() * grid.length);
-        }
-
-        return newFood;
-    };
-
-    let food = getFood();
+    let snake: number[] = [];
+    let direction: number;
+    let food: number;
 
     let lastUpdateTime = 0;
     const moveDelay = 125;
     let inputQueue: number[] = [1];
     let isGameRunning = false;
 
-    // Change direction with arrow keys
     const changeDirection = (e: { key: string }) => {
         const lastDirection = inputQueue[inputQueue.length - 1];
         let newDirection = lastDirection;
@@ -59,10 +47,20 @@
         requestAnimationFrame(gameLoop);
     };
 
+    const getFood = () => {
+        let newFood = Math.floor(Math.random() * grid.length);
+
+        while (snake.includes(newFood)) {
+            newFood = Math.floor(Math.random() * grid.length);
+        }
+
+        return newFood;
+    };
+
     const startGame = () => {
-        snake = [22, 21, 20];
+        snake = [2, 1, 0];
         direction = 1;
-        inputQueue = [1];
+        food = getFood();
         isGameRunning = true;
         lastUpdateTime = performance.now();
         requestAnimationFrame(gameLoop);
@@ -73,11 +71,9 @@
         alert("Game Over!");
     };
 
-    // Move snake
     const moveSnake = () => {
         const head = snake[0] + direction;
 
-        // Check for collisions
         if (
             head < 0 ||
             head >= grid.length ||
@@ -97,19 +93,9 @@
             snake.pop();
         }
     };
-
-    // Listen for arrow key presses
-    window.addEventListener("keydown", changeDirection);
-
-    // Reactive grid updates
-    // $: grid = grid.map((_, index) =>
-    //     snake.includes(index)
-    //         ? "snake" // Snake cells
-    //         : index === food
-    //           ? "food" // Food cell
-    //           : null
-    // );
 </script>
+
+<svelte:window on:keydown={changeDirection} />
 
 <div
     class="grid"
@@ -147,6 +133,9 @@
     }
     .cell.snake {
         background-color: green;
+    }
+    .cell.snake.head {
+        background-color: darkgreen;
     }
     .cell.food {
         background-color: red;
